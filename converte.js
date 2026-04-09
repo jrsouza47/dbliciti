7c4a95f0cf037324bbcf9305f20787f2f2575eb3
José Roberto <jrsouza47@gmail.com>
2026-04-09 18:20:24 -0300

    chore: remove arquivos temporarios do repositorio

--- converte.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-const b = fs.readFileSync('D:\\OneDrive\\Documentos\\Benner\\Trabalhos ZéNeto\\Correções do contrato Benner.pdf')
-fs.writeFileSync('contrato_base64.txt', b.toString('base64'))
-console.log('Arquivo gerado!')
